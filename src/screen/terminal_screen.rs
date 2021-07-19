94f422437f7bcac869ed77dcf9f565b35662cc47
dzosz <tomek3319@gmail.com>
2021-07-19 12:11:11 +0200

    change point type from x,y to Point(x,y) and from usize to i32. Reformat the file using rust formatter

--- src/screen/terminal_screen.rs
+++ src/screen/terminal_screen.rs
@@ -2,46 +2,54 @@ use crate::screen::Screen;
 
 use std::io::{self, Write};
 
-const WIDTH : usize = 950;
-const HEIGHT : usize= 670;
-const dW : usize= 8;
-const dH : usize= 8;
-static mut termWidth : usize = 80;
-static mut termHeight : usize = 24;
+const WIDTH: usize = 950;
+const HEIGHT: usize = 670;
+const dW: usize = 8;
+const dH: usize = 8;
+static mut termWidth: usize = 80;
+static mut termHeight: usize = 24;
 
 pub struct TerminalScreen {
-    canvas : [[bool; WIDTH]; HEIGHT],
-    x : f32,
-    y : f32,
-    zoom : f32,
-    _palette : i32,
+    canvas: [[bool; WIDTH]; HEIGHT],
+    x: f32,
+    y: f32,
+    zoom: f32,
+    _palette: i32,
 }
 
+struct Point(i32, i32);
+
 impl TerminalScreen {
-    pub fn new(x : f32, y : f32, z : f32) -> TerminalScreen {
-        let mut obj = TerminalScreen { canvas : [[false; WIDTH]; HEIGHT], x : x, y: y, zoom: z, _palette : 0 };
+    pub fn new(x: f32, y: f32, z: f32) -> TerminalScreen {
+        let mut obj = TerminalScreen {
+            canvas: [[false; WIDTH]; HEIGHT],
+            x: x,
+            y: y,
+            zoom: z,
+            _palette: 0,
+        };
         obj.Setup();
         obj.Clear();
         obj
     }
-    fn brightness(&self, count : usize) -> u8 {
-        let p : &'static [(usize, &str); 3]= &[(10, " .,:;oOQ#@"), (10, "     .oO@@"), (3, " .:")];
+    fn brightness(&self, count: usize) -> u8 {
+        let p: &'static [(usize, &str); 3] = &[(10, " .,:;oOQ#@"), (10, "     .oO@@"), (3, " .:")];
 
-        if 0 <= self._palette  && self._palette <=2 {
+        if 0 <= self._palette && self._palette <= 2 {
             let ref pal = p[self._palette as usize];
             pal.1.as_bytes()[count * (pal.0 - 1) / dW / dH]
         } else {
             ' ' as u8
         }
     }
-    fn FillScreenWithString(&mut self, frame : &[[u8; WIDTH/dW + 1]; HEIGHT/dH]) {
+    fn FillScreenWithString(&mut self, frame: &[[u8; WIDTH / dW + 1]; HEIGHT / dH]) {
         let mut out = io::stdout();
-        let lineheight = unsafe {std::cmp::min(termHeight as usize, HEIGHT/dH) };
+        let lineheight = unsafe { std::cmp::min(termHeight as usize, HEIGHT / dH) };
         let mut go_to_line_ansi_esacpe_code = String::new();
-        let linewidth = unsafe { std::cmp::min(termWidth, WIDTH / dW +1) };
+        let linewidth = unsafe { std::cmp::min(termWidth, WIDTH / dW + 1) };
 
         for line_idx in 0..lineheight {
-            go_to_line_ansi_esacpe_code = format!("{esc}[{};1H", line_idx, esc=27 as char);
+            go_to_line_ansi_esacpe_code = format!("{esc}[{};1H", line_idx, esc = 27 as char);
             out.write_all(go_to_line_ansi_esacpe_code.as_bytes());
             out.write_all(&frame[line_idx]);
         }
@@ -49,119 +57,142 @@ impl TerminalScreen {
     }
 
     fn Setup(&mut self) {
-        use terminal_size::{Width, Height, terminal_size};
+        use terminal_size::{terminal_size, Height, Width};
         let size = terminal_size();
         match size {
             Some((Width(w), Height(h))) => unsafe {
-                termWidth = w as usize; termHeight = h as usize;
+                termWidth = w as usize;
+                termHeight = h as usize;
                 println!("terminal w:{} h{}", termWidth, termHeight);
             },
             None => panic!("can't get terminal size"),
         }
     }
-    fn transform(&mut self, pos : &mut [i64; 2], x : f32, y: f32 ) {
+    fn transform(&mut self, x: f32, y: f32) -> Point {
         // from world to screen coordinates
-        let xx = ((x - self.x) as f32 * self.zoom) as i64 + (WIDTH as i64 / 2);
-        let yy = ((y - self.y) as f32* self.zoom) as i64 + (HEIGHT as i64 /2);
+        let xx = ((x - self.x) * self.zoom) as i32 + (WIDTH as i32 / 2);
+        let yy = ((y - self.y) * self.zoom) as i32 + (HEIGHT as i32 / 2);
 
-        pos[0] = HEIGHT as i64 -1 - yy as i64;
-        pos[1] = xx as i64;
+        let h = HEIGHT as i32 - 1 - yy as i32;
+        let w = xx as i32;
+        Point(h, w)
     }
-    fn drawPoint(&mut self, A : usize, B: usize ) {
-        if A < 0 || B < 0 || A >= HEIGHT || B>= WIDTH {
+    fn drawPoint(&mut self, point: Point) {
+        let (x, y) = (point.0, point.1);
+        if x < 0 || y < 0 || x >= HEIGHT as i32 || y >= WIDTH as i32 {
             return;
         }
-        self.canvas[A][B] = true;
+        self.canvas[x as usize][y as usize] = true;
     }
 
-    fn drawBoldPoint(&mut self, A : i64, B : i64) {
-        for i in A-1..=A+1 {
-            for j in B-1..=B+1 {
-                self.drawPoint(i as usize, j as usize); // TODO avoid under/overflow
+    fn drawBoldPoint(&mut self, point: Point) {
+        let (x, y) = (point.0, point.1);
+        for i in x - 1..=x + 1 {
+            for j in y - 1..=y + 1 {
+                self.drawPoint(Point(x, y));
             }
         }
     }
 
-    fn drawLine(&mut self, a : usize, b: usize, c : usize, d : usize) {
+    // Bresenham's line algorithm
+    fn drawLine(&mut self, a: Point, b: Point) {
         // sorting
-        let (mut A,mut B,mut C,mut D) = (a,b,c,d);
-        if A > C {
-            std::mem::swap(&mut A, &mut C);
-            std::mem::swap(&mut B, &mut D);
+        let mut fromPoint = a;
+        let mut toPoint = b;
+        if fromPoint.0 > toPoint.0 {
+            std::mem::swap(&mut fromPoint, &mut toPoint);
         }
 
         // algorithm
-        if B == D {
-            for i in A..=C {
-                self.drawBoldPoint(i as i64, B as i64);
+        if fromPoint.1 == toPoint.1 {
+            for i in fromPoint.0..=toPoint.0 {
+                self.drawBoldPoint(Point(i, fromPoint.1));
             }
             return;
         }
-        if A == C {
-            if D < B {
-                std::mem::swap(&mut B, &mut D);
+        if fromPoint.0 == toPoint.0 {
+            if toPoint.1 < fromPoint.1 {
+                std::mem::swap(&mut fromPoint.1, &mut toPoint.1);
             }
 
-            for i in B..=D {
-                self.drawBoldPoint(A as i64, i as i64);
+            for i in fromPoint.1..=toPoint.1 {
+                self.drawBoldPoint(Point(fromPoint.0, i));
             }
             return;
         }
-        if (D as i64-B as i64).abs() < (C as i64-A as i64).abs() {
-            self.drawLineLow(A,B,C,D);
+
+        let isGradientSoft = (toPoint.1 - fromPoint.1).abs() < (toPoint.0 - fromPoint.0).abs();
+        if isGradientSoft {
+            if fromPoint.0 > fromPoint.1 {
+                self.drawLineLow(toPoint, fromPoint);
+            } else {
+                self.drawLineLow(fromPoint, toPoint);
+            }
         } else {
-            if B > D { self.drawLineHigh(C,D,A,B); }
-            else { self.drawLineHigh(A,B,C,D); }
-        }
-    }
-
-    fn drawRectangle(&mut self, i1 : i64, j1: i64, i2 : i64, j2 : i64) {
-        let minI = std::cmp::min(i1, i2);
-        let maxI = std::cmp::max(i1, i2);
-        let minJ = std::cmp::min(j1, j2);
-        let maxJ = std::cmp::max(j1, j2);
-
-        for i in minI..=maxI {
-            for j in minJ..=maxJ {
-                self.drawPoint(i as usize, j as usize);
+            if fromPoint.1 > toPoint.1 {
+                self.drawLineHigh(toPoint, fromPoint);
+            } else {
+                self.drawLineHigh(fromPoint, toPoint);
             }
         }
     }
 
-    fn drawLineLow(&mut self, x0 : usize, y0: usize, x1: usize, y1: usize) {
-        let dx = x1 as i64 - x0 as i64;
-        let (dy, yi) = if y1 >= y0 { (y1 as i64-y0 as i64, 1i64) } else { (y0 as i64-y1 as i64, -1i64) };
+    fn drawRectangle(&mut self, fromPoint: Point, toPoint: Point) {
+        let minX = std::cmp::min(fromPoint.0, toPoint.0);
+        let maxX = std::cmp::max(fromPoint.0, toPoint.0);
+        let minY = std::cmp::min(fromPoint.1, toPoint.1);
+        let maxY = std::cmp::max(fromPoint.1, toPoint.1);
+
+        for x in minX..=maxX {
+            for y in minY..=maxY {
+                self.drawPoint(Point(x, y));
+            }
+        }
+    }
+
+    fn drawLineLow(&mut self, fromPoint: Point, toPoint: Point) {
+        let (x0, y0, x1, y1) = (fromPoint.0, fromPoint.1, toPoint.0, toPoint.1);
+        let dx = x1 - x0;
+        let (dy, yi) = if y1 >= y0 {
+            (y1 - y0, 1)
+        } else {
+            (y0 - y1, -1)
+        };
 
         let mut D = 2 * dy - dx;
-        let mut y = y0 as i64;
+        let mut y = y0;
 
         for x in x0..=x1 {
-            self.drawBoldPoint(x as i64, y);
+            self.drawBoldPoint(Point(x, y));
             if D > 0 {
                 y += yi;
-                D -= 2* dx;
+                D -= 2 * dx;
             }
             D += 2 * dy;
         }
     }
 
-    fn drawLineHigh(&mut self, x0 : usize, y0: usize, x1: usize, y1: usize) {
-        let dy = y1 as i64 - y0 as i64;
-        let (dx, xi) = if x1 >= x0 { (x1 as i64 -x0 as i64, 1i64) } else { (x0 as i64 -x1 as i64, -1i64) };
+    fn drawLineHigh(&mut self, fromPoint: Point, toPoint: Point) {
+        let (x0, y0, x1, y1) = (fromPoint.0, fromPoint.1, toPoint.0, toPoint.1);
+        let dy = y1 - y0;
+        let (dx, xi) = if x1 >= x0 {
+            (x1 - x0, 1)
+        } else {
+            (x0 - x1, -1)
+        };
 
         let mut D = 2 * dx - dy;
-        let mut x = x0 as i64;
+        let mut x = x0;
 
         for y in y0..=y1 {
-            self.drawBoldPoint(x, y as i64);
+            self.drawBoldPoint(Point(x, y));
             if D > 0 {
                 x += xi;
-                D -= 2* dy;
+                D -= 2 * dy;
             }
             D += 2 * dx;
         }
     }
-
 }
 impl Screen for TerminalScreen {
     fn Clear(&mut self) {
@@ -172,72 +203,64 @@ impl Screen for TerminalScreen {
         }
     }
 
-    fn PlotPoint(&mut self, x : f32, y :f32) {
-        let mut pos = [0; 2];
-        self.transform(&mut pos, x, y);
-        self.drawPoint(pos[0] as usize, pos[1] as usize);
+    fn PlotPoint(&mut self, x: f32, y: f32) {
+        let point = self.transform(x, y);
+        self.drawPoint(point);
     }
 
-    fn PlotLine(&mut self, x1 : f32, y1 :f32, x2 : f32, y2 : f32) {
-        let mut pos1 = [0; 2];
-        let mut pos2 = [0; 2];
-        self.transform(&mut pos1, x1, y1);
-        self.transform(&mut pos2, x2, y2);
-        self.drawLine(pos1[0] as usize, pos1[1] as usize, pos2[0] as usize, pos2[1] as usize);
+    fn PlotLine(&mut self, x1: f32, y1: f32, x2: f32, y2: f32) {
+        let p1 = self.transform(x1, y1);
+        let p2 = self.transform(x2, y2);
+        self.drawLine(p1, p2);
     }
 
-    fn PlotCircle(&mut self, x : f32, y: f32, r: f32) {
-        let mut p1 = [0; 2];
-        let mut p2 = [0; 2];
+    fn PlotCircle(&mut self, x: f32, y: f32, r: f32) {
+        let p1 = self.transform(x - r, y + r);
+        let p2 = self.transform(x + r, y - r);
 
-        self.transform(&mut p1, x - r, y + r);
-        self.transform(&mut p2, x + r, y - r);
-
-        for i in p1[0]..=p2[0] {
-            for j in p1[1]..=p2[1] {
-                let xt = (j as f32 - WIDTH as f32/2.0) / self.zoom + self.x as f32;
-                let yt = (HEIGHT as f32/2.0 - 1.0 - i as f32) / self.zoom + self.y as f32;
-                let radius2 = (xt-x) * (xt-x) + (yt - y) * (yt - y);
-                if radius2 <= r*r {
-                    self.drawPoint(i as usize, j as usize);
+        for i in p1.0..=p2.0 {
+            for j in p1.1..=p2.1 {
+                let xt = (j as f32 - WIDTH as f32 / 2.0) / self.zoom + self.x as f32;
+                let yt = (HEIGHT as f32 / 2.0 - 1.0 - i as f32) / self.zoom + self.y as f32;
+                let radius2 = (xt - x) * (xt - x) + (yt - y) * (yt - y);
+                let isInCircle = radius2 <= r * r;
+                if isInCircle {
+                    self.drawPoint(Point(i, j));
                 }
             }
         }
     }
 
-    fn PlotRectangle(&mut self, x1 : f32, y1 :f32, x2 : f32, y2 : f32) {
-        let mut p1 = [0; 2];
-        let mut p2 = [0; 2];
-
-        self.transform(&mut p1, x1, y1);
-        self.transform(&mut p2, x2, y2);
-        self.drawRectangle(p1[0], p1[1], p2[0], p2[1]);
+    fn PlotRectangle(&mut self, x1: f32, y1: f32, x2: f32, y2: f32) {
+        let p1 = self.transform(x1, y1);
+        let p2 = self.transform(x2, y2);
+        self.drawRectangle(p1, p2);
     }
 
-    fn Position(&mut self, x : f32, y : f32) {
+    fn Position(&mut self, x: f32, y: f32) {
         self.x = x;
         self.y = y;
     }
 
-    fn Zoom(&mut self, zoom : f32) {
+    fn Zoom(&mut self, zoom: f32) {
         self.zoom = zoom;
     }
 
     fn Draw(&mut self) {
-        let mut frame = [['x' as u8; WIDTH / dW + 1]; HEIGHT/dH];
-        for i in 0..HEIGHT/dH -1 {
-            frame[i][WIDTH/dW] = '\n' as u8;
+        let mut frame = [['x' as u8; WIDTH / dW + 1]; HEIGHT / dH];
+        for i in 0..HEIGHT / dH - 1 {
+            frame[i][WIDTH / dW] = '\n' as u8;
         }
-        frame[HEIGHT / dH - 1][WIDTH/dW] = '\0' as u8;
+        frame[HEIGHT / dH - 1][WIDTH / dW] = '\0' as u8;
         let mut countMax = 0;
 
-        for i in 0..HEIGHT/dH {
-            for j in 0..WIDTH/dW {
+        for i in 0..HEIGHT / dH {
+            for j in 0..WIDTH / dW {
                 let mut count = 0;
 
                 // calculating brightness
                 for k in 0..dH {
-                    for l in 0 ..dW {
+                    for l in 0..dW {
                         count += self.canvas[dH * i + k][dW * j + l] as usize;
                     }
                 }
@@ -246,21 +269,20 @@ impl Screen for TerminalScreen {
                 countMax = std::cmp::max(count, countMax);
             }
         }
-        
+
         // borders
-        for i in 0..HEIGHT/dH {
+        for i in 0..HEIGHT / dH {
             frame[i][0] = '@' as u8;
-            frame[i][WIDTH/dW - 1] = '@' as u8;
+            frame[i][WIDTH / dW - 1] = '@' as u8;
         }
-        for j in 0..WIDTH/dW {
+        for j in 0..WIDTH / dW {
             frame[0][j] = '@' as u8;
-            frame[HEIGHT/dH-1][j] = '@' as u8;
+            frame[HEIGHT / dH - 1][j] = '@' as u8;
         }
         self.FillScreenWithString(&frame);
     }
 
-    fn set_palette(&mut self, palette : i32) {
+    fn set_palette(&mut self, palette: i32) {
         self._palette = palette;
     }
-
 }
